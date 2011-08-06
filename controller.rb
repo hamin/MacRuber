91a9ccca2424a42d476e9307819c0d1b3c507d20
Haris Amin <aminharis7@gmail.com>
2011-08-06 03:47:28 -0400

    NSTableview finally setup with regex reference, also cleaned up drawer/table UI

--- controller.rb
+++ controller.rb
@@ -22,17 +22,45 @@ class Controller
 	
 	
   def awakeFromNib
+    @data = []
+    @data << [ '[abc]', 'A single character: a, b or c', '.', 'Any single character', '(...)', 'Capture everything enclosed' ]
+    @data << [ '[^abc]', 'Any single character but: a, b or c', '\s', 'Any whitespace character', '(a|b)', 'a or b' ]
+    @data << [ '[a-z]', 'Any single character in the range a-z', '\S', 'Any non-whitespace character', 'a?', 'Zero or one of a' ]
+    @data << [ '[a-zA-Z]', 'Any single character in the range a-z of A-Z', '\d', 'Any digit', 'a*', 'Zero or more of a' ]
+    @data << [ '[^]', 'Start of line', '\D', 'Any non-digit', 'a+', 'One or more of a' ]
+    @data << [ '[$]', 'End of line', '\w', 'Any word character (letter, number, underscore)', 'a{3}', 'Exactly 3 of a' ]
+    @data << [ '[\A]', 'Start of string', '\W', 'Any non-word character', 'a{3,}', '3 or more of a' ]
+    @data << [ '[\z]', 'End of string', '\b', 'Any word boundary character', 'a{3,6}', 'Between 3 and 6 of a' ]
 		@referenceTableView.dataSource = self
   end
 	
 	def numberOfRowsInTableView(view)
-    2
+    # @data.size
+    8
   end
+  
+  # def numberOfColumnsInTableView(view)
+  #   6
+  # end
 	
   def tableView(view, objectValueForTableColumn:column, row:index)
-	  "foo"
-		puts column.inspect
-		NSLog "data: #{column}"
+    row_data = @data[index]
+    
+    case column.identifier
+    when "col0"
+      row_data[0]
+    when "col1"
+      row_data[1]
+    when "col2"
+      row_data[2]
+    when "col3"
+      row_data[3]
+    when "col4"
+      row_data[4]
+    when "col5"
+      row_data[5]         
+    end
+    
   end
 	
 	def controlTextDidChange(notification)

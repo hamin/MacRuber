32bb0b4bd136d3863919a9b49e972e313bb44b96
Haris Amin <aminharis7@gmail.com>
2011-08-09 04:12:48 -0400

    regex reference table now properly inserts regex in each row in the textfield, with appropriate characters selected within the textfield

--- controller.rb
+++ controller.rb
@@ -6,7 +6,7 @@
 
 class Controller
   attr_accessor :testString, :regex, :bwLabel, :matchedString, :window
-	attr_writer :referenceTableView
+	attr_accessor :referenceTableView
 
   def evalRegex(sender)
 		begin
@@ -22,31 +22,31 @@ class Controller
 	
 	
   def awakeFromNib
-    @ref_data = []
-    @ref_data << [ '[abc]', 'A single character: a, b or c' ] 
-    @ref_data << [ '.', 'Any single character' ]
-    @ref_data << [ '(...)', 'Capture everything enclosed' ]
-    @ref_data << [ '[^abc]', 'Any single character but: a, b or c', '\s' ]
-    @ref_data << [ '\s', 'Any whitespace character' ]
-    @ref_data << [ '(a|b)', 'a or b' ]
-    @ref_data << [ '[a-z]', 'Any single character in the range a-z' ]
-    @ref_data << [ '\S', 'Any non-whitespace character' ]
-    @ref_data << [ 'a?', 'Zero or one of a' ]
-    @ref_data << [ '[a-zA-Z]', 'Any single character in the range a-z of A-Z' ]
-    @ref_data << [ '\d', 'Any digit' ]
-    @ref_data << [ 'a*', 'Zero or more of a' ]
-    @ref_data << [ '[^]', 'Start of line' ]
-    @ref_data << [ '\D', 'Any non-digit' ]
-    @ref_data << [ 'a+', 'One or more of a' ]
-    @ref_data << [ '[$]', 'End of line' ]
-    @ref_data << [ '\w', 'Any word character (letter, number, underscore)' ]
-    @ref_data << [ 'a{3}', 'Exactly 3 of a' ]
-    @ref_data << [ '[\A]', 'Start of string' ]
-    @ref_data << [ '\W', 'Any non-word character' ]
-    @ref_data << [ 'a{3,}', '3 or more of a' ]
-    @ref_data << [ '[\z]', 'End of string' ]
-    @ref_data << [ '\b', 'Any word boundary character' ]
-    @ref_data << [ 'a{3,6}', 'Between 3 and 6 of a' ]
+    @ref_data = [] # [ value, description, selection range ]
+    @ref_data << [ '[abc]', 'A single character: a, b or c', { :skip => 1, :include => 3} ] 
+    @ref_data << [ '.', 'Any single character', nil ]
+    @ref_data << [ '(...)', 'Capture everything enclosed', {:skip => 1, :include => 3} ]
+    @ref_data << [ '[^abc]', 'Any single character but: a, b or c', {:skip => 2, :include => 3} ]
+    @ref_data << [ '\s', 'Any whitespace character', nil ]
+    @ref_data << [ '(a|b)', 'a or b', {:skip => 1, :include => 3} ]
+    @ref_data << [ '[a-z]', 'Any single character in the range a-z', {:skip => 1, :include => 3} ]
+    @ref_data << [ '\S', 'Any non-whitespace character', nil ]
+    @ref_data << [ 'a?', 'Zero or one of a', {:skip => 0, :include => 1} ]
+    @ref_data << [ '[a-zA-Z]', 'Any single character in the range a-z of A-Z', {:skip => 1, :include => 6} ]
+    @ref_data << [ '\d', 'Any digit', nil ]
+    @ref_data << [ 'a*', 'Zero or more of a', {:skip => 0, :include => 1} ]
+    @ref_data << [ '[^]', 'Start of line', nil ] ### MAYBE???
+    @ref_data << [ '\D', 'Any non-digit', nil ]
+    @ref_data << [ 'a+', 'One or more of a', {:skip => 0, :include => 1} ]
+    @ref_data << [ '[$]', 'End of line', nil ] ### MAYBE???
+    @ref_data << [ '\w', 'Any word character (letter, number, underscore)', nil ]
+    @ref_data << [ 'a{3}', 'Exactly 3 of a', {:skip => 0, :include => 1} ]
+    @ref_data << [ '[\A]', 'Start of string', nil ] ### MAYBE???
+    @ref_data << [ '\W', 'Any non-word character', nil ]
+    @ref_data << [ 'a{3,}', '3 or more of a', {:skip => 0, :include => 1} ]
+    @ref_data << [ '[\z]', 'End of string', nil ] ### MAYBE???
+    @ref_data << [ '\b', 'Any word boundary character', nil ]
+    @ref_data << [ 'a{3,6}', 'Between 3 and 6 of a', {:skip => 0, :include => 1} ]
 
 		@referenceTableView.dataSource = self
   end
@@ -66,6 +66,21 @@ class Controller
     end
     
   end
+  
+  def tableViewSelectionDidChange(notification)
+    selected_row = @referenceTableView.selectedRow
+    selected_regex = @ref_data[selected_row][0]
+    old_regex = @regex.stringValue
+    new_regex = old_regex + selected_regex
+    @regex.stringValue = new_regex
+    range_info = @ref_data[selected_row][2]
+
+    unless range_info.nil?
+      @regex.selectText self
+      range = NSMakeRange( old_regex.length + range_info[:skip], range_info[:include] )
+      @regex.currentEditor.setSelectedRange range
+    end  
+  end  
 	
 	def controlTextDidChange(notification)
 		evalRegex(self)
